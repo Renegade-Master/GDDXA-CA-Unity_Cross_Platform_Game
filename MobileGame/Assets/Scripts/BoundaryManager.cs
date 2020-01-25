d1ed03ab6ddd6b6d2e7270fead593a00acc04d8f
Ciaran Bent [K00221230] <k00221230@student.lit.ie>
2020-01-25 19:47:14 +0000

    Commit before restructure;

---
+++ MobileGame/Assets/Scripts/BoundaryManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public class BoundaryManager : MonoBehaviour {
+    public float xMin, xMax, zMin, zMax;
+
+    // Handle an Object ENTERING the collider
+    private void OnTriggerEnter(Collider other) {
+        throw new NotImplementedException();
+    }
+
+    // Handle and Object EXITING the collider
+    private void OnTriggerExit(Collider other) {
+        throw new NotImplementedException();
+    }
+}
---
+++ MobileGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public float tilt;
+    public BoundaryManager boundary;
+
+    public GameObject shot;
+    public Transform shotSpawn;
+
+    public float fireRate;
+    private float nextFire;
+
+    private void Awake() {
+        boundary = GameObject.FindGameObjectWithTag("PlayArea").GetComponent<BoundaryManager>();
+    }
+
+    void Update() {
+        /*if (Input.GetButton ("Fire1") && Time.time > nextFire) {
+            nextFire = Time.time + fireRate;
+            Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+            GetComponent<AudioSource>().Play();
+        }*/
+    }
+
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+        
+        /*Debug.Log("Horizontal: " + moveHorizontal);
+        Debug.Log("Vertical: " + moveVertical);*/
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        GetComponent<Rigidbody>().velocity = movement * speed;
+
+        // Clamp the Player to within the bounds of the screen
+        GetComponent<Rigidbody>().position = new Vector3 (
+            Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
+            0,
+            Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+        );
+        GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+    }
+}
