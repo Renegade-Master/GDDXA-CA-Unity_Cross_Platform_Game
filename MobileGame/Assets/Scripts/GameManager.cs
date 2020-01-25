49d3007b0c47e027d0b86be6f05d069737dcf200
Ciaran Bent [K00221230] <k00221230@student.lit.ie>
2020-01-25 21:12:09 +0000

    File Restructure Successful; Setup GameWindow; Successfully created basic game infrastructure;

--- MobileGame/Assets/Scripts/GameManager.cs
+++ MobileGame/Assets/Scripts/GameManager.cs
@@ -1,16 +1,47 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
+    public int currentLevel;
+    
+    public GameObject playerPrefab;
+    public GameObject playAreaPrefab;
+    public GameObject[] skyBoxes;
+    
+    public GameObject player;
+    public GameObject playArea;
+    
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        // Place all starting GameObjects
+        LoadLevel(currentLevel);
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
+    }
+
+    void LoadLevel(int level) {
+        switch (level) {
+            case 0:
+                Instantiate(skyBoxes[level], Vector3.zero, Quaternion.Euler(Vector3.zero));
+                playArea = Instantiate(playAreaPrefab, Vector3.zero, Quaternion.Euler(Vector3.zero));
+                player = Instantiate(playerPrefab);
+                
+                player.GetComponent<Rigidbody>().transform.position = Vector3.zero;
+                player.GetComponent<Rigidbody>().transform.rotation = Quaternion.Euler(-90, 0, 90);
+                break;
+            default:
+                return;
+        }
+    }
+
+    void NextLevel() {
+        if (currentLevel >= 1) {
+            currentLevel = 0;
+        } else {
+            currentLevel++;
+        }
     }
 }
--- MobileGame/Assets/Scripts/PlayerController.cs
+++ MobileGame/Assets/Scripts/PlayerController.cs
@@ -3,43 +3,32 @@
 public class PlayerController : MonoBehaviour {
 
     public float speed;
-    public float tilt;
-    public BoundaryManager boundary;
-
     public GameObject shot;
-    public Transform shotSpawn;
-
-    public float fireRate;
-    private float nextFire;
+    
+    private BoundaryManager _boundary;
+    [SerializeField] Transform _shotSpawn;
 
     private void Awake() {
-        boundary = GameObject.FindGameObjectWithTag("PlayArea").GetComponent<BoundaryManager>();
+        _boundary = GameObject.FindGameObjectWithTag("PlayArea").GetComponent<BoundaryManager>();
     }
 
     void Update() {
-        /*if (Input.GetButton ("Fire1") && Time.time > nextFire) {
-            nextFire = Time.time + fireRate;
-            Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
-            GetComponent<AudioSource>().Play();
-        }*/
+        
     }
 
     void FixedUpdate() {
         float moveHorizontal = Input.GetAxis ("Horizontal");
         float moveVertical = Input.GetAxis ("Vertical");
-        
-        /*Debug.Log("Horizontal: " + moveHorizontal);
-        Debug.Log("Vertical: " + moveVertical);*/
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         GetComponent<Rigidbody>().velocity = movement * speed;
 
         // Clamp the Player to within the bounds of the screen
         GetComponent<Rigidbody>().position = new Vector3 (
-            Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
+            Mathf.Clamp(GetComponent<Rigidbody>().position.x, _boundary.xMin, _boundary.xMax),
             0,
-            Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+            Mathf.Clamp(GetComponent<Rigidbody>().position.z, _boundary.zMin, _boundary.zMax)
         );
-        GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+        //GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
     }
 }
